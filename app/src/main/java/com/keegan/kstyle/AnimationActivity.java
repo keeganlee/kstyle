cc86fc7cef839d0b5edee5984a5cad627e43377a
Keegan Lee <keegan.lee704@gmail.com>
2015-10-26 16:03:31 +0800

    add Property Animation

--- app/src/main/java/com/keegan/kstyle/AnimationActivity.java
+++ app/src/main/java/com/keegan/kstyle/AnimationActivity.java
@@ -15,6 +15,11 @@
  */
 package com.keegan.kstyle;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.IntEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,6 +28,8 @@ import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class AnimationActivity extends AppCompatActivity {
 
@@ -69,5 +76,66 @@ public class AnimationActivity extends AppCompatActivity {
 
     public void onMoveAndScale(View view) {
         view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.move_and_scale));
+    }
+
+    public void onValueAnimatorTest(final View view) {
+        // 获取屏幕宽度
+        final int maxWidth = getWindowManager().getDefaultDisplay().getWidth();
+        ValueAnimator valueAnimator = (ValueAnimator) AnimatorInflater.loadAnimator(this, R.animator.value_animator);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            private IntEvaluator mEvaluator = new IntEvaluator();
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                // 当前动画值，即为当前宽度比例值
+                int currentValue = (Integer) animator.getAnimatedValue();
+                // 根据比例更改目标view的宽度
+                view.getLayoutParams().width = maxWidth * currentValue / 100;
+                view.requestLayout();
+            }
+        });
+        valueAnimator.start();
+    }
+
+    public void onObjectAnimatorTest(View view) {
+        // 获取屏幕宽度
+        int maxWidth = getWindowManager().getDefaultDisplay().getWidth();
+        ViewWrapper wrapper = new ViewWrapper(view, maxWidth);
+        ObjectAnimator objectAnimator = (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.object_animator);
+        objectAnimator.setTarget(wrapper);
+        objectAnimator.start();
+    }
+
+    public void onAnimatorSetTest(View view) {
+        // 获取屏幕宽度
+        int maxWidth = getWindowManager().getDefaultDisplay().getWidth();
+        ViewWrapper wrapper = new ViewWrapper(view, maxWidth);
+        AnimatorSet animatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.animator_set);
+        animatorSet.setTarget(wrapper);
+        animatorSet.start();
+    }
+
+    private static class ViewWrapper {
+        private View target;
+        private int maxWidth;
+
+        public ViewWrapper(View target, int maxWidth) {
+            this.target = target;
+            this.maxWidth = maxWidth;
+        }
+
+        public int getWidth() {
+            return target.getLayoutParams().width;
+        }
+
+        public void setWidth(int widthValue) {
+            target.getLayoutParams().width = maxWidth * widthValue / 100;
+            target.requestLayout();
+        }
+
+        public void setMarginTop(int margin) {
+            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) target.getLayoutParams();
+            layoutParams.setMargins(0, margin, 0, 0);
+            target.setLayoutParams(layoutParams);
+        }
     }
 }
